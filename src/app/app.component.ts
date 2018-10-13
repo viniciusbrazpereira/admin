a20a5e63d2d2c77b5f1a63b83ec2b7719d5e909c
Vinicius Braz Pereira <vinicius@jaya.tech>
2018-10-13 00:24:14 -0300

    Angular Material.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export interface Food {
+  	value: string;
+  	viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'admin';
+	title = 'admin';
+	
+	foods: Food[] = [
+	    {value: 'steak-0', viewValue: 'Steak'},
+	    {value: 'pizza-1', viewValue: 'Pizza'},
+	    {value: 'tacos-2', viewValue: 'Tacos'}
+	];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatSelectModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
